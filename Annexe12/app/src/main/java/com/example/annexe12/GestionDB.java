c33b0c8f84c5935394f373b2eea277cb512869f4
yanis3000 <yanimosoruz@outlook.com>
2025-10-07 17:59:45 -0400

    feat : annexe12

--- Annexe12/app/src/main/java/com/example/annexe12/GestionDB.java
+++ Annexe12/app/src/main/java/com/example/annexe12/GestionDB.java
@@ -10,25 +10,25 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class OperationBD extends SQLiteOpenHelper {
-    private static OperationBD instance;
+public class GestionDB extends SQLiteOpenHelper {
+    private static GestionDB instance;
     private SQLiteDatabase database; // un moyen de l'obtenir APRES avoir ouvert l'activite
 
-    public static OperationBD getInstance( Context contexte) {
+    public static GestionDB getInstance(Context contexte) {
         if ( instance == null ) {
-            instance = new OperationBD(contexte);
+            instance = new GestionDB(contexte);
         }
         return instance;
     }
 
-    private OperationBD(@Nullable Context context) {
+    private GestionDB(@Nullable Context context) {
         super(context, "BD", null, 1);
     }
 
     /* ATTENTION APPELER UNE SEULE FOIS LORS DE L'INSTALLATION DE L'APP SUR L'APPAREIL */
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase db) { // important : EXECUTE UNE SEULE FOIS LORS DE L'INSTALLATION DE L'APP
         db.execSQL("CREATE TABLE inventeur ( _id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, origine TEXT, invention TEXT, annee INTEGER)");
         ajouterInventeur(new Inventeur("Laszlo Biro", "Hongrie", "Stylo a bille", 1938), db);
         ajouterInventeur(new Inventeur("Benjamin Franklin", "USA", "Paratonnerre", 1752), db);
@@ -56,6 +56,33 @@ public class OperationBD extends SQLiteOpenHelper {
         database = getWritableDatabase();
     }
 
+    public boolean aBonneReponse(String invention, String nom) {
+        String[] tab = {nom, invention};
+
+        Cursor c = database.rawQuery("SELECT invention FROM inventeur WHERE nom = ? AND invention = ?", tab);
+        //si vide, moveToFirst va retouner faux sinon ca retourne true
+        boolean rep = c.moveToFirst();
+        c.close();
+        return rep; // ce n'est pas une bonne maniere de proceder
+
+    }
+
+    // trouver l'indice de la bonne reponse etant donne un nom d'inventeur
+
+    public int trouverIndiceBonneReponse(String nom) throws Exception {
+        String[] tab = {nom};
+        Cursor c = database.rawQuery("SELECT _id FROM inventeur WHERE nom = ?", tab);
+        if (c.moveToFirst()) {
+            int rep = c.getInt(0) -  1; // les _id commencent a 0
+            c.close();
+            return rep;
+        }
+        else {
+            throw new Exception("Le nom de l'inventeur n'est pas dans la table");
+        }
+
+    }
+
     public void fermerConnectionDB() {
         database.close();
     }
@@ -63,9 +90,12 @@ public class OperationBD extends SQLiteOpenHelper {
     public ArrayList<String> retournerInventions() {
         ArrayList<String> liste = new ArrayList<>();
         Cursor c = database.rawQuery("SELECT invention FROM inventeur", null);
-        while (c.moveToFirst()) {
-            liste.add(c.getString(0));
+        while (c.moveToNext()) { // tant qu'il y a des resultats
+            liste.add(c.getString(0)); // j'ajoute l'invention dans le vector, je veux tous les nommer
         }
+
+//        if (c.moveToLast()) return c.getString(0); // scaphandre
+        c.close();
         return liste;
     }
 
--- Annexe12/app/src/main/java/com/example/annexe12/MainActivity.java
+++ Annexe12/app/src/main/java/com/example/annexe12/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.annexe12;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView texte;
+    ListView liste;
+    GestionDB instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        texte = findViewById(R.id.texte);
+        liste = findViewById(R.id.liste);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        instance = GestionDB.getInstance(getApplicationContext()); // dans l'idee que c'est un singleton
+        // this foncitonne aussi
+        instance.ouvrirConnectionDB();
+//        choix = instance.retournerInventions(), on aurait pu faire ca
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, instance.retournerInventions());
+        liste.setAdapter(adapter);
+        Ecouteur ec = new Ecouteur();
+        liste.setOnItemClickListener(ec); // spinner : OnItemSelectedListener
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        instance.fermerConnectionDB();
+    }
+
+    private class Ecouteur implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+//            if (view == liste.getChildAt(2))  texte.setText("Bonne reponse");
+//            else texte.setText("Mauvaise reponse");
+
+//            String itemSelectionne = instance.retournerInventions().get(position);
+//
+            String itemSelectionne = (String) liste.getItemAtPosition(position);
+
+                if (instance.aBonneReponse(itemSelectionne, "Mary Anderson")) {
+                    texte.setText("oui");
+                    view.setBackgroundColor(Color.GREEN);
+                } else {
+                    texte.setText("non");
+                    view.setBackgroundColor(Color.RED);
+                    try {
+                        liste.getChildAt(instance.trouverIndiceBonneReponse("Mary Anderson")).setBackgroundColor(Color.GREEN);
+                    } catch (Exception e) {
+//                        throw new RuntimeException(e);
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                liste.setOnItemClickListener(null);
+
+
+        }
     }
 }
